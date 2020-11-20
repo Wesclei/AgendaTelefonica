70792c8d564803141989962ba81a662b43516e52
Wesclei Araujo <wescleiaraujo2@hotmail.com>
2020-11-19 22:10:00 -0300

    Fim do projeto Agênda.

---
+++ Controllers/PessoasController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgendaTelefonica.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTelefonica.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly Contexto _contexto;
+
+        public PessoasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Pessoas.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CriarPessoa()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarPessoa(Pessoa pessoa)
+        {
+            if (ModelState.IsValid) {
+                _contexto.Add(pessoa);
+                await _contexto.SaveChangesAsync();
+                TempData["ContatoNovo"] = $"Contato com o nome {pessoa.Nome} incluído com sucesso !";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(pessoa);
+            }            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarPessoa(int pessoaId)
+        {
+            Pessoa pessoa = await _contexto.Pessoas.FindAsync(pessoaId);
+
+            if (pessoa == null)
+                return NotFound();
+            return View(pessoa);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AtualizarPessoa(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(pessoa);
+                await _contexto.SaveChangesAsync();
+                TempData["ContatoAtualizado"] = $"Contato {pessoa.Nome} atualizado com sucesso !";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ExcluirPessoa(int pessoaId)
+        {
+            Pessoa pessoa = await _contexto.Pessoas.FindAsync(pessoaId);
+            _contexto.Pessoas.Remove(pessoa);
+            await _contexto.SaveChangesAsync();
+            TempData["ContatoExcluido"] = $"Contato {pessoa.Nome} excluído com sucesso !";
+            return Json(true);
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaTelefonica.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public string Telefone { get; set; }
+                
+        public string Celular { get; set; }        
+    }
+
+}
